41ece705e38a2a58db8fd0910059a85455dbb653
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2024-08-05 17:48:08 -0300

    first commit - startup

---
+++ src/main/java/rpinfo/challenge/rpinfo_project/RpinfoProjectApplication.java
@@ -0,0 +1,13 @@
+package rpinfo.challenge.rpinfo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpinfoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpinfoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rpinfo/challenge/rpinfo_project/RpinfoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rpinfo.challenge.rpinfo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpinfoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
